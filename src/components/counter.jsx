6b4a46a8b2af7516dda8e89b1411ab4259a1a7ce
Datian <zdatian@gmail.com>
2018-12-24 12:17:09 -0500

    increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,35 @@
 // _cer->
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1
+  };
 
+  updateValue = product => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
-      <h1>Hello World</h1>
+      <div>
+        <span className={classes}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.updateValue({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
 }
-
 export default Counter;
